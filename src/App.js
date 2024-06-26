cdeb60fe4fa05a0c9f743406d0c2361381c1056c
lepperson11 <lepperson11@gmail.com>
2024-06-26 13:08:29 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootswatch/dist/darkly/bootstrap.css";
+import Navbar from "./components/Navbar/Navbar";
+import { Route, Routes } from "react-router-dom";
+import RecordList from "./components/RecordList";
+import Create from "./components/Create";
+import Edit from "./components/Edit";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<RecordList />} />
+        <Route exact path="/students/create" element={<Create />} />
+        <Route exact path="/students/Edit/:id" element={<Edit />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./loader.scss";
+const Loader = () => {
+  return <tr className="loader"></tr>;
+};
+
+export default Loader;
